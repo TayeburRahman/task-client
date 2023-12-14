90e953ae650c62a613e65cafd1a46a84ccd78f5a
TayeburRahman <tayebrayhan101@gmail.com>
2023-12-14 22:26:00 +0600

    update

--- src/components/shared/Footer.js
+++ src/components/shared/Footer.js
@@ -4,7 +4,7 @@ function Footer() {
     return (
         <div className='container_ pt-5 pb-2'>
               <hr/>
-            <p className='pt-2'>Copyright © 2023 - All Rights Reserved</p>
+            <p className='pt-2'>2023 Tayebur Rahman - All Rights Reserved</p>
         </div>
     )
 }
